efdb163fb7c87c1851858bc851a6a1c157f0145c
nithyaw <vishnuvardhan2050@gmail.com>
2026-03-30 14:38:24 +0530

    created main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+print("Hi Everyone")
+
+def add(a, b):
+    return a + b
+
+print(add(5, 10))
+
+def sub(a, b):
+    return a - b
+
+print(sub(10, 5))
+
+def mul(a, b):
+    return a * b
+
+print(mul(5, 10))
+
+def div(a, b):
+    # Note: In Python 3, this returns a float (2.0)
+    return a / b
+
+print(div(10, 5))
