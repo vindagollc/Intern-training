dd1202f8cfa97e59583d8a0df8c7c321268b2185
abinav04 <abhinavsosa23@gmail.com>
2026-03-30 14:05:38 +0530

    My commit

---
+++ intern.js
@@ -0,0 +1,25 @@
+console.log("Hi Everyone");
+
+function add(a,b){
+    return a+b;
+}
+
+console.log(add(5,10));
+
+function sub(a,b){
+    return a-b;
+}
+
+console.log(sub(10,5));
+
+function mul(a,b){
+    return a*b;
+}
+
+console.log(mul(5,10));
+
+function div(a,b){
+    return a/b;
+}       
+
+console.log(div(10,5));
